6f29bf04074729e762bde81ce495d674aea42a20
Big_Lee <oscarlee0509@gmail.com>
2021-05-19 07:09:03 +0800

    mokito spy

--- src/test/java/com/biglee/schedulerthread/controller/repoControllerTest.java
+++ src/test/java/com/biglee/schedulerthread/controller/repoControllerTest.java
@@ -1,9 +1,13 @@
 package com.biglee.schedulerthread.controller;
 
+import com.biglee.schedulerthread.entity.Home;
+import com.biglee.schedulerthread.repo.HomeRepo;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -14,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 /**
  * @author BigLee E-mail:biglee001@hotmail.com
@@ -27,9 +32,19 @@ public class repoControllerTest {
 
     @Resource
     private MockMvc mockMvc;
+    @Resource
+    HomeRepo repo;
+
     @Test
     @DisplayName("返回值校验")
     void login() throws Exception {
+        HomeRepo spy= Mockito.spy(repo);
+        Home home = new Home();
+        home.setId("2");
+        Mockito.when(spy.findById("1")).thenReturn(java.util.Optional.of(home));
+        Optional<Home> optional = spy.findById("1");
+        Assertions.assertEquals("2",optional.get().getId());
+
         mockMvc.perform(MockMvcRequestBuilders.get("/save") //执行请求
                 .accept(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk())
